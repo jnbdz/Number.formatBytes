dd6950d7a286068395ff77a0ebc443b1db9a7c59
Jean-Nicolas Boulay <contact@jean-nicolas.com>
2012-11-15 04:34:52 -0500

    First Commit

---
+++ Source/Number.formatBytes.js
@@ -0,0 +1,19 @@
+/*
+---
+description: 
+
+authors:
+  - Jean-Nicolas Boulay (http://jean-nicolas.com/)
+
+license:
+  - MIT-style license
+
+requires:
+ - core/1.4
+
+provides:
+  - Number.formatBytes
+...
+*/
+
+var Number.formatBytes = new Class({});
